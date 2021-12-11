9bb6f0ec4f1d7d06ebb102c6e925717529b8e176
Mihail Bogdanchikov <mihailBogdanchikovWork@gmail.com>
2021-12-11 06:02:30 +0300

    fix

--- src/main/java/ru/home/usertableapp/util/DbUtil.java
+++ src/main/java/ru/home/usertableapp/util/DbUtil.java
@@ -6,7 +6,7 @@ import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
+
 
 public class DbUtil {
     private static Connection connection;
